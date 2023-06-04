52ad732f09beb26b54ee0386a4d9446a642aa7f5
andy123t <andy123t@163.com>
2023-06-04 11:25:13 +0800

    update

--- makefiles.sh
+++ makefiles.sh
@@ -8,6 +8,7 @@ echo "${FileName%.tex}"
 #-> Compile the main file
 xelatex --synctex=-1 "$FileName"
 bibtex "${FileName%.tex}"
+makeindex "${FileName%.tex}"
 xelatex --synctex=-1 "$FileName"
 xelatex --synctex=-1 "$FileName"
 #-> Clear aux files
